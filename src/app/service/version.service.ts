b5b6ded87492d6a656319ee7d331c5380e5f83bc
jablo-dev <mail@jablo.dev>
2026-06-22 16:52:00 +0200

    Added patchnotes for version 4.4.0

--- src/app/service/version.service.ts
+++ src/app/service/version.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class VersionService {
-    private readonly version = '4.3.2';
+    private readonly version = '4.4.0';
 
     getVersion(): string {
         return this.version;
